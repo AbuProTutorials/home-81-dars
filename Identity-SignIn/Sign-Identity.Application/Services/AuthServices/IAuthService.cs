e6dfd574da04d2ce15faf2447639d21852b4f533
Abu_Programmiy <abdukholiq0907@gmail.com>
2024-04-23 09:15:33 +0500

    tugadi

---
+++ Identity-SignIn/Sign-Identity.Application/Services/AuthServices/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Sign_Identity.Domain.DTOs;
+using Sign_Identity.Domain.Entities.Auth;
+
+namespace Sign_Identity.Application.Services.AuthServices
+{
+    public interface IAuthService
+    {
+        public Task<AuthDTO> GenerateToken(User user);
+    }
+}
